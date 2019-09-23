3bbf0a68a7bed5152d2a437bad527da9952dd376
TomokiIzu <kooomt.d13@gmail.com>
2019-09-23 08:31:27 +0000

    Rails04 課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-
+  validates :name, presence: true,length: { minimum: 1, maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+      
   has_secure_password
-end
+  validates :password, presence: true, format: { with: /\A[0-9A-Za-z]{8,32}\z/ }
+  
+end 
