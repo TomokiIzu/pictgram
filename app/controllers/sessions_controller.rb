a1bdf3248ae2270f14fa02331ce86ad6bc745b7d
TomokiIzu <kooomt.d13@gmail.com>
2019-09-26 11:52:05 +0000

    ログインできない問題

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email_parms)
-    if user && user.authenticate(password_params)
+    user = User.find_by(email: email_parms)
+    if user && user.authenticate(password_parms)
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
